0495f7518efdeb31057b2003dd32a32fcfd3c152
sealsrar <57424474+sealsrar@users.noreply.github.com>
2025-08-25 20:36:03 -0300

    deu certo

---
+++ Radio_H7/CM7/Core/Inc/nrf.h
@@ -0,0 +1,163 @@
+/*
+ * nrf.h
+ *
+ *  Created on: Aug 15, 2025
+ *      Author: udavi
+ */
+
+#ifndef INC_NRF_H_
+#define INC_NRF_H_
+
+#include "stm32h7xx_hal.h"
+
+/**
+ * @name Commands
+ * @anchor commands
+ * @anchor command
+ *
+ * Comandos do NRF24L01
+ *
+ * Tabela 19 na página 48 do datasheet
+ *
+**/
+#define NRF_CMD_R_REGISTER                0x00
+#define NRF_CMD_W_REGISTER                0x20
+#define NRF_CMD_R_RX_PAYLOAD              0x61
+#define NRF_CMD_W_TX_PAYLOAD              0xA0
+#define NRF_CMD_FLUSH_TX                  0xE1
+#define NRF_CMD_FLUSH_RX                  0xE2
+#define NRF_CMD_REUSE_TX_PL               0xE3
+#define NRF_CMD_R_RX_PL_WID               0x60
+#define NRF_CMD_W_ACK_PAYLOAD             0xA8
+#define NRF_CMD_W_TX_PAYLOAD_NO_ACK       0xB0
+#define NRF_CMD_NOP                       0xFF
+
+
+/**
+ * @name Registers
+ * @anchor registers
+ * @anchor register
+ *
+ * Registradores do NRF24
+ *
+ * Tabela 27 dapágina 57 do datasheet
+ */
+//!@{
+#define NRF_REG_CONFIG      0x00
+#define NRF_REG_EN_AA       0x01
+#define NRF_REG_EN_RXADDR   0x02
+#define NRF_REG_SETUP_AW    0x03
+#define NRF_REG_SETUP_RETR  0x04
+#define NRF_REG_RF_CH       0x05
+#define NRF_REG_RF_SETUP    0x06
+#define NRF_REG_STATUS      0x07
+#define NRF_REG_OBSERVE_TX  0x08
+#define NRF_REG_RPD         0x09
+#define NRF_REG_RX_ADDR_P0  0x0A
+#define NRF_REG_RX_ADDR_P1  0x0B
+#define NRF_REG_RX_ADDR_P2  0x0C
+#define NRF_REG_RX_ADDR_P3  0x0D
+#define NRF_REG_RX_ADDR_P4  0x0E
+#define NRF_REG_RX_ADDR_P5  0x0F
+#define NRF_REG_TX_ADDR     0x10
+#define NRF_REG_RX_PW_P0    0x11
+#define NRF_REG_RX_PW_P1    0x12
+#define NRF_REG_RX_PW_P2    0x13
+#define NRF_REG_RX_PW_P3    0x14
+#define NRF_REG_RX_PW_P4    0x15
+#define NRF_REG_RX_PW_P5    0x16
+#define NRF_REG_FIFO_STATUS 0x17
+#define NRF_REG_DYNPD       0x1C
+#define NRF_REG_FEATURE     0x1D
+//!@}
+
+// Bits de configuração
+#define CFG_BIT_EN_CRC  0x3 //Posicção do Bit de CRC no registro de power do NRF
+#define CFG_BIT_PWR_UP  0x1 //Posição do Bit de Power up no registro de power do NRF
+#define CFG_BIT_PRIM_RX 0x0 //Posição do Bit de Rx/Tx no registro de power do NRF (0 = Tx, 1 = Rx) <- VALOR NO REGISTRO NAO POSIÇÃO
+//define dos estados do nrf
+#define STATUS_BIT_RX_DR   0x6
+#define STATUS_BIT_TX_DS   0x5
+#define STATUS_BIT_MAX_RT  0x4
+#define STATUS_BIT_RX_P_NO 0x1 // consists of bits 1-3
+#define STATUS_BIT_TX_FULL 0x0
+
+#define NRF_SPI_TIMEOUT_TIME 10
+
+typedef enum
+{
+  NRF_OK              = 0x00, //!< All went well
+  NRF_SPI_ERROR       = 0x01, //!< Unspecified SPI error
+  NRF_SPI_BUSY        = 0x02, //!< SPI device busy
+  NRF_SPI_TIMEOUT     = 0x03, //!< SPI communication timed out
+  NRF_ERROR           = 0x04, //!< Unspecified general error
+  NRF_MAX_RT          = 0x05, //!< Max retries on packet transmission
+  NRF_BAD_TRANSITION  = 0x06  //!< When an unallowed mode transition is tried
+} NRF_Status;
+
+typedef enum
+{
+  NRF_Mode_Powerdown = 0x00,
+  NRF_Mode_Standby = 0x01,
+  NRF_Mode_RxMode = 0x02,
+  NRF_Mode_TxMode = 0x03
+} NRF_Mode;
+
+
+typedef struct
+{
+	/* SPI */
+	SPI_HandleTypeDef *spiHandle;
+	GPIO_TypeDef *csPinBank;
+	GPIO_TypeDef *cePinBank;
+	uint16_t csPin;
+	uint16_t cePin;
+	/* Modo */
+	NRF_Mode Mode;
+	/* DMA */
+
+} NRF24;
+
+void wait(uint64_t us);
+
+void Set_NRF24(NRF24 *nrf, SPI_HandleTypeDef *handle, GPIO_TypeDef *PortCS, uint16_t PinCs, GPIO_TypeDef *PortCE, uint16_t PinCE);
+
+NRF_Status NRF24_SendCommand(NRF24 *nrf, uint8_t cmd);
+
+NRF_Status NRF24_SendWriteCommand(NRF24 *nrf, uint8_t cmd , uint8_t *write, uint8_t length);
+
+NRF_Status NRF24_SendReadCommand(NRF24 *nrf, uint8_t cmd, uint8_t *read, uint8_t length);
+
+NRF_Status NRF24_WriteRegister(NRF24 *nrf,uint8_t reg, uint8_t *write, uint8_t Length);
+
+NRF_Status NRF24_WriteRegisterByte(NRF24 *nrf,uint8_t reg, uint8_t byte);
+
+NRF_Status NRF24_ReadRegister(NRF24 *nrf,uint8_t reg, uint8_t *read, uint8_t Length);
+
+uint8_t NRF24_ReadRegisterByte(NRF24 *nrf, uint8_t byte);
+
+NRF_Status NRF24_SetRegisterBit(NRF24 *nrf, uint8_t reg, uint8_t bit);
+
+NRF_Status NRF24_ResetRegisterBit(NRF24 *nrf, uint8_t reg, uint8_t bit);
+
+uint8_t NRF_ReadStatus(NRF24 *nrf);
+
+NRF_Status NRF24_EnterMode(NRF24 *nrf, uint8_t mode);
+
+NRF_Status NRF24_Config(NRF24 *nrf);
+
+void Tx_mode(NRF24 *nrf, uint8_t Address[5]);
+
+void Rx_mode(NRF24 *nrf, uint8_t Address[5], uint8_t dataSize);
+
+NRF_Status NRF24_WritePayload(NRF24 *nrf, uint8_t *payload, uint8_t length);
+
+NRF_Status NRF24_TransmitAndWait(NRF24 *nrf, uint8_t *payload, uint8_t length);
+
+NRF_Status NRF24_Transmit(NRF24 *nrf, uint8_t *payload, uint8_t length);
+
+NRF_Status NRF24_ReadPayload(NRF24 *nrf, uint8_t *read, uint8_t length);
+
+NRF_Status ReceiveData(NRF24 *nrf, uint8_t *data, uint32_t len) ;
+
+#endif /* INC_NRF_H_ */
---
+++ Radio_H7/CM7/Core/Src/nrf.c
@@ -0,0 +1,322 @@
+/*
+ * nrf.c
+ *
+ *  Created on: Aug 15, 2025
+ *      Author: udavi
+ */
+
+#include "nrf.h"
+
+uint32_t CPU_Freq = 0x00; //Variável para salvar a frequência da CPU
+
+void wait(uint64_t us) {
+	uint32_t volatile cycles = CPU_Freq * us / 1000000; //Quantos ciclos de CPU deverão ser esperados para alcancar tal tempo
+	uint32_t volatile current = 0; //Quantos se passaram
+	while (current <= cycles) {
+		current++;
+	}
+}
+
+void Set_NRF24(NRF24 *nrf, SPI_HandleTypeDef *handle, GPIO_TypeDef *PortCS,
+		uint16_t PinCS, GPIO_TypeDef *PortCE, uint16_t PinCE) {
+	nrf->spiHandle = handle;
+	nrf->csPinBank = PortCS;
+	nrf->cePinBank = PortCE;
+	nrf->csPin = PinCS;
+	nrf->cePin = PinCE;
+	nrf->Mode = NRF_Mode_Powerdown;
+
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET);// Garante que o Select está em nível alto
+	HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET);// Garante que o enable está em nível baixo
+	CPU_Freq = HAL_RCC_GetSysClockFreq();
+
+	HAL_Delay(100);
+}
+
+NRF_Status NRF24_SendCommand(NRF24 *nrf, uint8_t cmd) {
+	NRF_Status ret = NRF_OK;
+	uint8_t status;
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_RESET); //csn_reset()
+	ret = (NRF_Status) HAL_SPI_TransmitReceive(nrf->spiHandle, &cmd, &status, 1,
+			NRF_SPI_TIMEOUT_TIME); //Envia o comando e retorna o status ********************************************
+	if (ret != NRF_OK) { //Verifica se o comando foi enviado corretamente
+		return ret;
+		HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET); //csn_set()
+	}
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET); //csn_set()
+	return ret;
+}
+
+NRF_Status NRF24_SendWriteCommand(NRF24 *nrf, uint8_t cmd, uint8_t *write,
+		uint8_t length) {
+	NRF_Status ret = NRF_OK;
+	uint8_t status;
+
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_RESET); //cs_reset
+	ret = (NRF_Status) HAL_SPI_TransmitReceive(nrf->spiHandle, &cmd, &status, 1,
+	NRF_SPI_TIMEOUT_TIME);
+	if(ret != NRF_OK){
+		HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET);
+		return ret;
+	}
+	ret = (NRF_Status) HAL_SPI_Transmit(nrf->spiHandle, write, length,
+	NRF_SPI_TIMEOUT_TIME);
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET);
+	return ret;
+}
+
+NRF_Status NRF24_SendReadCommand(NRF24 *nrf, uint8_t cmd, uint8_t *read,
+		uint8_t length) {
+	NRF_Status ret = NRF_OK;
+	uint8_t status;
+
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_RESET);
+
+	ret = (NRF_Status) HAL_SPI_TransmitReceive(nrf->spiHandle, &cmd, &status, 1,
+	NRF_SPI_TIMEOUT_TIME);
+	if(ret != NRF_OK){
+		HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET);
+		return ret;
+	}
+	ret = (NRF_Status) HAL_SPI_Receive(nrf->spiHandle, read, length,
+	NRF_SPI_TIMEOUT_TIME);
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET);
+	return ret;
+}
+
+NRF_Status NRF24_WriteRegister(NRF24 *nrf, uint8_t reg, uint8_t *write,
+		uint8_t Length) {
+	return NRF24_SendWriteCommand(nrf, NRF_CMD_W_REGISTER | reg, write, Length);
+}
+
+NRF_Status NRF24_WriteRegisterByte(NRF24 *nrf, uint8_t reg, uint8_t byte) {
+	uint8_t write = byte;
+	return NRF24_WriteRegister(nrf, reg, &write, 1);
+}
+
+NRF_Status NRF24_ReadRegister(NRF24 *nrf, uint8_t reg, uint8_t *read,
+		uint8_t Length) {
+	return NRF24_SendReadCommand(nrf, NRF_CMD_R_REGISTER | reg, read, Length);
+}
+
+uint8_t NRF24_ReadRegisterByte(NRF24 *nrf, uint8_t reg) {
+	uint8_t read;
+	NRF24_SendReadCommand(nrf, NRF_CMD_R_REGISTER | reg, &read, 1);
+	return read;
+}
+
+NRF_Status NRF24_SetRegisterBit(NRF24 *nrf, uint8_t reg, uint8_t bit) {
+	NRF_Status ret = NRF_OK;
+	uint8_t cfg = 0x00;
+
+	ret = NRF24_ReadRegister(nrf, reg, &cfg, 1);
+	if (ret != NRF_OK) {
+		return ret;
+	}
+	cfg = cfg | (1 << bit);
+	return NRF24_WriteRegister(nrf, reg, &cfg, 1);
+}
+
+NRF_Status NRF24_ResetRegisterBit(NRF24 *nrf, uint8_t reg, uint8_t bit) {
+	NRF_Status ret = NRF_OK;
+	uint8_t cfg = 0x00;
+
+	ret = NRF24_ReadRegister(nrf, reg, &cfg, 1); //Pega a informação do byte do registro
+	if (ret != NRF_OK) {
+		return ret;
+	}
+
+	cfg = cfg & ~(1 << bit); //Altera o bit por meio de um E binário
+	return NRF24_WriteRegister(nrf, reg, &cfg, 1); //Escreve o registro de volta
+}
+
+uint8_t NRF24_ReadStatus(NRF24 *nrf) {
+	uint8_t status = 0x00;
+	uint8_t cmd = NRF_CMD_NOP;
+
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_RESET);
+	HAL_SPI_TransmitReceive(nrf->spiHandle, &cmd, &status, 1,
+	NRF_SPI_TIMEOUT_TIME);
+	HAL_GPIO_WritePin(nrf->csPinBank, nrf->csPin, GPIO_PIN_SET);
+
+	return status;
+}
+
+NRF_Status NRF24_EnterMode(NRF24 *nrf, NRF_Mode modo){
+	NRF_Status ret = NRF_OK;
+	switch(modo){
+	case(NRF_Mode_Powerdown):
+			HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET);
+			ret = NRF24_ResetRegisterBit(nrf, NRF_REG_CONFIG, CFG_BIT_PWR_UP);
+			nrf->Mode = NRF_Mode_Powerdown;
+			break;
+	case(NRF_Mode_Standby):
+			if(nrf->Mode == NRF_Mode_Powerdown){
+				ret = NRF24_SetRegisterBit(nrf, NRF_REG_CONFIG, CFG_BIT_PWR_UP);
+				wait(1500);
+			} else if(nrf->Mode == NRF_Mode_TxMode){
+				HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET);
+			} else if(nrf->Mode == NRF_Mode_RxMode){
+				ret = NRF24_ResetRegisterBit(nrf, NRF_REG_CONFIG, CFG_BIT_PRIM_RX);
+				HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET);
+			}
+			nrf->Mode = NRF_Mode_Standby;
+			break;
+	case(NRF_Mode_TxMode):
+			if(nrf->Mode != NRF_Mode_Standby){
+				ret = NRF_BAD_TRANSITION;
+			} else {
+				ret = NRF24_ResetRegisterBit(nrf, NRF_REG_CONFIG, CFG_BIT_PRIM_RX); // 0 = Tx
+				HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_SET); //ce_set();
+				wait(10);
+				nrf->Mode = NRF_Mode_TxMode;
+			}
+			break;
+	case(NRF_Mode_RxMode):
+			if(nrf->Mode != NRF_Mode_Standby){
+					ret = NRF_BAD_TRANSITION;
+				} else {
+					ret = NRF24_SetRegisterBit(nrf, NRF_REG_CONFIG, CFG_BIT_PRIM_RX); // 0 = Tx
+					HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_SET); //ce_set();
+					nrf->Mode = NRF_Mode_RxMode;
+				}
+			break;
+	}
+	return ret;
+}
+
+NRF_Status NRF24_Config(NRF24 *nrf){
+	NRF_Status ret = NRF_OK;
+	NRF24_EnterMode(nrf, NRF_Mode_Powerdown);
+	NRF24_EnterMode(nrf, NRF_Mode_Standby);
+
+	// Flush FIFOs
+	NRF24_EnterMode(nrf, NRF_Mode_TxMode);
+	NRF24_SendCommand(nrf, NRF_CMD_FLUSH_TX);
+	NRF24_EnterMode(nrf, NRF_Mode_Standby);
+	NRF24_EnterMode(nrf, NRF_Mode_RxMode);
+	NRF24_SendCommand(nrf, NRF_CMD_FLUSH_RX);
+	NRF24_EnterMode(nrf, NRF_Mode_Standby);
+
+	NRF24_WriteRegisterByte(nrf, NRF_REG_CONFIG, 0x0A); // 00001010
+	NRF24_WriteRegisterByte(nrf, NRF_REG_EN_AA, 0x3F); // 00011111 = AutoAcknologment desligado em todos os Pipes
+	NRF24_WriteRegisterByte(nrf, NRF_REG_EN_RXADDR, 0x03); //00000011 -> Pipes 0 e 1 no Rx
+	NRF24_WriteRegisterByte(nrf, NRF_REG_SETUP_AW, 0x03); //00000011 -> 5 bytes no adresss
+	NRF24_WriteRegisterByte(nrf, NRF_REG_SETUP_RETR, 0x0F); //00000011 -> re-transmit desabilitado
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RF_CH, 0x02); //00000010 -> Canal 3
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RF_SETUP, 0x0e); //00001110 -> LNA desligado, 0dBm, 2MBs
+	NRF24_WriteRegisterByte(nrf, NRF_REG_STATUS, 0x70); // clear flags
+
+	uint8_t address[5] = {1,2,3,4,5};
+	uint8_t address2[5] = {2,3,4,5,6};
+	NRF24_WriteRegister(nrf, NRF_REG_RX_ADDR_P0, address, 5); //Adress do pipe 0
+	NRF24_WriteRegister(nrf, NRF_REG_RX_ADDR_P1, address2, 5); //Adress pipe 1
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_ADDR_P2, 0xC3);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_ADDR_P3, 0xC4);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_ADDR_P4, 0xC5);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_ADDR_P5, 0xC6);
+	NRF24_WriteRegister(nrf, NRF_REG_TX_ADDR, address, 5);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P0, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P1, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P2, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P3, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P4, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P5, 0x00);
+
+	NRF24_WriteRegisterByte(nrf, NRF_REG_FIFO_STATUS, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_DYNPD, 0x00);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_FEATURE, 0x00);
+	return ret;
+}
+
+void Tx_mode(NRF24 *nrf, uint8_t Adress[5]) {
+	NRF24_Config(nrf);
+	NRF24_WriteRegister(nrf, NRF_REG_TX_ADDR, Adress, 5);
+	//NRF24_EnterMode(nrf, NRF_Mode_TxMode);
+	 //ce_reset();
+	//NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P0, dataSize);
+	NRF24_WriteRegister(nrf,NRF_REG_RX_ADDR_P0,Adress,5);
+	//HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET);
+}
+
+void Rx_mode(NRF24 *nrf, uint8_t Adress[5], uint8_t dataSize) {
+	NRF24_Config(nrf);
+	NRF24_WriteRegister(nrf, NRF_REG_RX_ADDR_P0, Adress, 5);
+	NRF24_WriteRegisterByte(nrf, NRF_REG_RX_PW_P0, dataSize);
+	NRF24_EnterMode(nrf, NRF_Mode_RxMode);
+	wait(130);
+}
+
+NRF_Status NRF24_WritePayload(NRF24 *nrf, uint8_t *payload, uint8_t length) {
+	return NRF24_SendWriteCommand(nrf, NRF_CMD_W_TX_PAYLOAD, payload, length);
+}
+
+
+NRF_Status NRF24_TransmitAndWait(NRF24 *nrf, uint8_t *payload, uint8_t length) {
+	NRF_Status ret = NRF_OK;
+
+	ret = NRF24_WritePayload(nrf, payload, length);
+	if (ret != NRF_OK) {
+
+		return ret;
+	}
+
+	// Transmit
+	HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_SET); //ce_set();
+
+	wait(130);
+
+	// Wait for status update
+	uint8_t status;
+	for (;;) {
+		status = NRF24_ReadStatus(nrf);
+		if (status & (1 << STATUS_BIT_TX_DS)) {
+			// Packet transmitted
+			ret = NRF24_SetRegisterBit(nrf, NRF_REG_STATUS, STATUS_BIT_TX_DS); // clear flag
+			break;
+		} else if (status & (1 << STATUS_BIT_MAX_RT)) {
+			// Max retransmits reached
+			NRF24_SetRegisterBit(nrf, NRF_REG_STATUS, STATUS_BIT_MAX_RT); // clear flag
+			ret = NRF_MAX_RT;
+			break;
+		}
+	}
+	HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET); //ce_reset();
+
+	return ret;
+}
+
+NRF_Status NRF24_Transmit(NRF24 *nrf, uint8_t *payload, uint8_t length) {
+	NRF_Status ret = NRF_OK;
+	ret = NRF24_WritePayload(nrf, payload, length);
+	if (ret != NRF_OK) {
+		return ret;
+	}
+
+
+	HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_SET); //ce_set();
+	wait(130);
+	HAL_GPIO_WritePin(nrf->cePinBank, nrf->cePin, GPIO_PIN_RESET); //ce_reset();
+
+	return ret;
+}
+
+NRF_Status NRF24_ReadPayload(NRF24 *nrf, uint8_t *read, uint8_t length) {
+	return NRF24_SendReadCommand(nrf, NRF_CMD_R_RX_PAYLOAD, read, length);
+}
+
+NRF_Status ReceiveData(NRF24 *nrf, uint8_t *data, uint32_t len) {
+	NRF_Status ret = NRF_ERROR;
+	uint8_t status = NRF24_ReadStatus(nrf);
+	uint8_t STATUS_REGISTER_RX_DR_BIT = 6;
+	if (status & (1 << STATUS_REGISTER_RX_DR_BIT)) {
+		NRF24_ReadPayload(nrf, data, len);
+		ret = NRF_OK;
+		NRF24_SetRegisterBit(nrf, NRF_REG_STATUS, 6);
+	} else {
+		ret = NRF_ERROR;
+	}
+	return ret;
+}
+
+
